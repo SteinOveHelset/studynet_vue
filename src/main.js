aaad907a418369a1b15b5ecab8e1f5811eee8e9f
Code With Stein <codewithstein@gmail.com>
2021-08-14 20:59:50 +0200

    part 1

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+createApp(App).use(store).use(router, axios).mount('#app')
