f85c29fe1a9544689ee4657dbbd2fadacea2c83f
Code With Stein <codewithstein@gmail.com>
2021-11-26 21:23:03 +0100

    Part 9

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -14,10 +14,21 @@
                             <p class="menu-label">Categories</p>
 
                             <ul class="menu-list">
-                                <li><a class="is-active">All courses</a></li>
-                                <li><a>Programming</a></li>
-                                <li><a>Design</a></li>
-                                <li><a>UX</a></li>
+                                <li>
+                                    <a 
+                                        v-bind:class="{'is-active': !activeCategory}"
+                                        @click="setActiveCategory(null)"
+                                    >
+                                        All courses
+                                    </a>
+                                </li>
+                                <li
+                                    v-for="category in categories"
+                                    v-bind:key="category.id"
+                                    @click="setActiveCategory(category)"
+                                >
+                                    <a>{{ category.title }}</a>
+                                </li>
                             </ul>
                         </aside>
                     </div>
@@ -36,18 +47,6 @@
                                 <nav class="pagination">
                                     <a class="pagination-previous">Previous</a>
                                     <a class="pagination-next">Next</a>
-
-                                    <ul class="pagination-list">
-                                        <li>
-                                            <a class="pagination-link is-current">1</a>
-                                        </li>
-                                        <li>
-                                            <a class="pagination-link">2</a>
-                                        </li>
-                                        <li>
-                                            <a class="pagination-link">3</a>
-                                        </li>
-                                    </ul>
                                 </nav>
                             </div>
                         </div>
@@ -66,22 +65,49 @@ import CourseItem from '@/components/CourseItem.vue'
 export default {
     data() {
         return {
-            courses: []
+            courses: [],
+            categories: [],
+            activeCategory: null
         }
     },
     components: {
         CourseItem
     },
-    mounted() {
+    async mounted() {
         console.log('mounted')
 
-        axios
-            .get('/api/v1/courses/')
+        await axios
+            .get('/api/v1/courses/get_categories/')
             .then(response => {
                 console.log(response.data)
 
-                this.courses = response.data
+                this.categories = response.data
             })
+        
+        this.getCourses()
+    },
+    methods: {
+        setActiveCategory(category) {
+            console.log(category)
+            this.activeCategory = category
+
+            this.getCourses()
+        },
+        getCourses() {
+            let url = '/api/v1/courses/'
+
+            if (this.activeCategory) {
+                url += '?category_id=' + this.activeCategory.id
+            }
+
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data)
+
+                    this.courses = response.data
+                })
+        }
     }
 }
 </script>
